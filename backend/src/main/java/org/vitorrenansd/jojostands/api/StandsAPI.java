eebdbb58d934a86cd418d6e37af33551acfc5726
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-15 11:15:56 -0300

    Changed GET from /stands/random to /stand

--- backend/src/main/java/org/vitorrenansd/jojostands/api/StandsAPI.java
+++ backend/src/main/java/org/vitorrenansd/jojostands/api/StandsAPI.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/stands")
+@RequestMapping(value = "/stand")
 public class StandsAPI {
     private final StandsFacade standsFacade;
 
@@ -16,17 +16,12 @@ public class StandsAPI {
     }
 
     @GetMapping
-    public List<StandsDTO> getAll() {
-        return standsFacade.getAll();
-    }
-
-    @GetMapping("/random")
     public StandsDTO getRandom() {
         return standsFacade.getRandom();
     }
-
-    @PostMapping
-    public void create(@RequestBody StandsDTO stand) {
-        standsFacade.addStand(stand);
+    
+    @GetMapping("/all")
+    public List<StandsDTO> getAll() {
+        return standsFacade.getAll();
     }
 }

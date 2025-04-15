b22d1a175cf20f818797cb68540ac174c561d863
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-15 07:53:22 -0300

    Initial commit

---
+++ backend/src/main/java/org/vitorrenansd/jojostands/JojoStandsAPI.java
@@ -0,0 +1,13 @@
+package org.vitorrenansd.jojostands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojoStandsAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JojoStandsAPI.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/vitorrenansd/jojostands/JojostandsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vitorrenansd.jojostands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojostandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

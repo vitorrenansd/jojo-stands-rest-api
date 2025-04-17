5c8682d0f34a02648e6d8a4d28adf688d2750cd4
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-17 09:45:13 -0300

    Hotfix on StandsAPI, added CORS

--- backend/src/main/java/org/vitorrenansd/jojostands/api/StandsAPI.java
+++ backend/src/main/java/org/vitorrenansd/jojostands/api/StandsAPI.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
  * @since 2025-04-15
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class StandsAPI {
     private final StandsFacade standsFacade;
 
